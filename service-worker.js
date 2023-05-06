7e811743c951c5c30196d1b7bb9efc7a5a1680db
Luiz <lhvalentim@outlook.coom>
2023-05-06 15:49:56 -0300

    fix cache

--- service-worker.js
+++ service-worker.js
@@ -3,31 +3,35 @@ self.addEventListener('install', event => {
 })
 
 self.addEventListener('fetch', event => {
-	event.respondWith(caches.open('diely')
-	.then(cache => {
-		return cache.match(event.request)
-		.then(cachedResponse => {
-			let cachedFile = cachedResponse.clone().blob()
-			const fetchedResponse = fetch(event.request)
-			.then(networkResponse => {
-				if (networkResponse.status == 200) cache.put(event.request, networkResponse.clone())
-				Promise.all([
-					cachedFile,
-					networkResponse.clone().blob()
-				]).then(response => {
-					if (!event.clientId) return
-					if (response[0].size == response[1].size) return
-					self.clients.get(event.clientId)
-					.then(client => {
-						client?.postMessage('update')
-					})
-				})
-				return networkResponse
+	event.respondWith(
+		caches.open('diely')
+		.then(cache => {
+			return cache.match(event.request)
+			.then(cachedResponse => {
+				fetchedResponse = fetchNewData(event, cache)
+				return cachedResponse || fetchedResponse
 			})
-			return cachedResponse || fetchedResponse
 		})
-	})
-	.catch(() => {
-		return fetch(event.request)
-	}))
+		.catch(() => {
+			return fetch(event.request)
+		})
+	)
 })
+
+function fetchNewData(event, cache, cachedFile) {
+	return fetch(event.request)
+	.then(networkResponse => {
+		if (networkResponse.status == 200) cache.put(event.request, networkResponse.clone())
+		if (cachedFile && event.clientId) {
+			networkResponse.clone().blob()
+			.then(response => {
+				if (response.size === cachedFile.size) return
+				self.clients.get(event.clientId)
+				.then(client => {
+					client?.postMessage('update')
+				})
+			})
+		}
+		return networkResponse
+	})
+}
